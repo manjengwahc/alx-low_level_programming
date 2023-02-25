07e649cee3a1f6a159e9ca9c7944f3d151621d0a
manjengwahc <manjengwahc@live.com>
2023-02-24 18:34:07 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -15,4 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
+#endif
